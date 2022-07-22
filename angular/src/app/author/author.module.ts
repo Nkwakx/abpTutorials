668400dae510f39791bb1f9f1335cb442d25b438
unknown <siphenathi@developmenthub.co.za>
2022-07-22 08:48:47 +0200

    final_commit

---
+++ angular/src/app/author/author.module.ts
@@ -0,0 +1,13 @@
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
+
+import { AuthorRoutingModule } from './author-routing.module';
+import { AuthorComponent } from './author.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [AuthorComponent],
+  imports: [SharedModule, AuthorRoutingModule, NgbDatepickerModule]
+})
+export class AuthorModule { }
---
+++ aspnet-core/src/Acme.BookStore.EntityFrameworkCore/Migrations/20220719074426_IniBook.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acme.BookStore.Migrations
+{
+    public partial class IniBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Books",
+                table: "Books");
+
+            migrationBuilder.RenameTable(
+                name: "Books",
+                newName: "AppBooks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AppBooks",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppBooks",
+                table: "AppBooks",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppBooks",
+                table: "AppBooks");
+
+            migrationBuilder.RenameTable(
+                name: "AppBooks",
+                newName: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Books",
+                table: "Books",
+                column: "Id");
+        }
+    }
+}
